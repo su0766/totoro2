6748378c2c7c9b29f260d5795db7f9839ea80f5a
tjangus2166 <tjangus2166@gmail.com>
2018-02-04 00:18:32 +0900

    init

---
+++ app/src/main/java/com/example/kimtuna/totoro2/ChoiceResult.java
@@ -0,0 +1,13 @@
+package com.example.kimtuna.totoro2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoiceResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice_result);
+    }
+}
